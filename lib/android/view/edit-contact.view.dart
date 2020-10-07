a6a2355075baaad3956c4f4fdee9867c878ebe7c
Gilson <gilsonjuniorpro@gmail.com>
2020-10-07 12:40:01 -0400

    model created

---
+++ lib/android/view/edit-contact.view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class EditContactView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
+
+
