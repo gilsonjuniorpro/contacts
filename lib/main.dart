ef3b990edca628b6b8e7af7edad0e6cfd02fb365
Gilson <gilsonjuniorpro@gmail.com>
2020-10-07 14:11:18 -0400

    ios version started

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 import 'android/android.app.dart';
+import 'ios/ios.app.dart';
 
 void main() {
   if(Platform.isIOS){
-
+    runApp(IOSApp());
   }else{
     runApp(AndroidApp());
   }
